416387527157732655fcd11eeeccea2006589786
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-05-13 17:53:59 +0100

    q8

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -65,10 +65,11 @@ p users["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
 p users["Erik"][:lottery_numbers].min()
 # 6. Return an array of Avril's lottery numbers that are even
-p users["Avril"][:lottery_numbers].select(&:even?)
+p users["Avril"][:lottery_numbers].select{ |lotto_num| lotto_num.even? }
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 p users["Erik"][:lottery_numbers].push(7)
 # 8. Change Erik's hometown to Edinburgh
 p users["Erik"][:home_town] = "Edinburgh"
 # 9. Add a pet dog to Erik called "Fluffy"
+p users["Erik"][:pets][0][:species]
 # 10. Add another person to the users hash
